48654e3f4b454f8055e6da494cdbcd1fb916ecd5
atreus-oss <ferreira14arturo@gmail.com>
2024-09-30 21:01:57 -0400

    Semana 3 - Tarea de Estructuras de Control

--- Tarea de Estructuras de Control/2-estructurasDeControl.js
+++ Tarea de Estructuras de Control/2-estructurasDeControl.js
@@ -4,7 +4,7 @@
 // Instrucción: Escribe una función que reciba un número y verifique si es par o impar.
 // Imprime "El número es par" o "El número es impar" según corresponda.
 function verificarParidad(numero) {
-    let numero = 4
+    numero = 4
     if (numero % 2 == 0){
         console.log("El numero es par")
     }else{
@@ -16,7 +16,7 @@ function verificarParidad(numero) {
 // Instrucción: Escribe una función que reciba una edad y determine si la persona es:
 // "Menor de edad" (menor a 18), "Adulto" (entre 18 y 65), o "Adulto mayor" (mayor a 65).
 function clasificarEdad(edad) {
-    let edad = 24
+    edad = 24
     if (edad >= 18 && edad <= 65){
         console.log("Usted es mayor de edad")
     }else if(edad >= 65){
@@ -29,7 +29,7 @@ function clasificarEdad(edad) {
 // Ejercicio 3: Bucles while
 // Instrucción: Crea una función que reciba un número entero positivo y utilice un bucle `while` para imprimir todos los números desde ese número hasta 0.
 function cuentaRegresiva(numero) {
-    let numero = 10
+    numero = 10
     while (numero > 0){
         console.log(numero)
         numero--
@@ -49,7 +49,7 @@ function repetirMensaje() {
 // Ejercicio 5: Bucle for
 // Instrucción: Escribe una función que reciba un número entero positivo y utilice un bucle `for` para imprimir todos los números pares entre 0 y ese número.
 function imprimirPares(numero) {
-    let numero = 10
+    numero = 10
     for (i = 0; i <= 10; i++){
         if (i % 2 == 0){
             console.log(i)
@@ -82,7 +82,7 @@ function saltarCinco() {
 // Ejercicio 8: Switch básico
 // Instrucción: Escribe una función que reciba un número del 1 al 7 y devuelva el día de la semana correspondiente (1 es "Lunes", 2 es "Martes", ..., 7 es "Domingo"). Usa la estructura `switch`.
 function obtenerDiaSemana(dia) {
-    let dia = 1
+    dia = 2
     switch (dia) {
         case 1:
             console.log("Hoy es Domingo")
@@ -113,7 +113,7 @@ function obtenerDiaSemana(dia) {
 // Ejercicio 9: Switch con múltiples casos
 // Instrucción: Escribe una función que reciba un carácter y devuelva si es una vocal. Utiliza un `switch` y agrupa los casos para las vocales (a, e, i, o, u).
 function esVocal(letra) {
-    let letra = "a"
+    letra = "a"
     switch (letra){
         case "a":
             console.log("La letra ingesada es la vocal 'a'")
@@ -138,7 +138,7 @@ function esVocal(letra) {
 // Ejercicio 10: Condicionales complejos con operadores lógicos
 // Instrucción: Escribe una función que reciba tres números y determine si todos son positivos, al menos uno es negativo, o todos son negativos. Usa operadores lógicos (`&&`, `||`).
 function evaluarNumeros(a, b, c) {
-    let a = 1; b = 2; c = 3
+    a = -1; b = 2; c = -3
 
     if (a > 0 && b > 0 && c > 0){
         console.log("Todos los numeros son positivos")
