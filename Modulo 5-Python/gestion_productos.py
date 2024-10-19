ca9ee4e80bb3ed853a10a0f1c38de3ad62323e65
atreus-oss <ferreira14arturo@gmail.com>
2024-10-19 16:04:18 -0300

    Tarea 5 - Python

--- Modulo 5-Python/gestion_productos.py
+++ Modulo 5-Python/gestion_productos.py
@@ -1,57 +1,3 @@
-'''def puntuacion_y_comentario():
-    while True:
-        print('Por favor, introduzca una puntuación en una escala de 1 a 5')
-        point = input()
-
-        if point.isdecimal():
-            point = int(point)
-
-            if point <= 0 or point > 5:  # Expresión condicional que verifica si es menor que 0 o mayor que 5
-                print('Indíquelo en una escala de 1 a 5')
-            else:
-                print('Por favor, introduzca un comentario')
-                comment = input()
-                post = f'punto: {point} comentario: {comment}'
-                file_pc = open("data.txt", 'a')
-                file_pc.write(f'{post} \n')
-                file_pc.close()
-                break
-        else:
-            print('Por favor, introduzca la puntuación en números')
-
-def resultados():
-    print('Resultados hasta la fecha.')
-    read_file = open("data.txt", "r")
-    print(read_file.read())
-    read_file.close()
-
-def main():
-    while True:
-        print('Seleccione el proceso que desea aplicar')
-        print('1: Ingresar puntuación y comentario')
-        print('2: Comprueba los resultados obtenidos hasta ahora.')
-        print('3: Finalizar')
-        num = input()
-
-        if num.isdecimal():
-            num = int(num)
-            if num == 1:
-                puntuacion_y_comentario()
-
-            if num == 2:
-                resultados()
-
-            if num == 3:
-                print('Finalizando')
-                break  # Sentencia para finalizar el procesamiento
-
-            else:
-                print('Introduzca un número del 1 al 3')
-        else:
-            print('Introduzca un número del 1 al 3')
-
-main()'''
-
 productos = []
 
 
@@ -140,7 +86,7 @@ def cargar_datos():
 
 
 def menu():
-    cargar_datos()  # Cargar datos al iniciar el programa
+    cargar_datos()
     while True:
         print("\n1: Añadir producto")
         print("2: Ver productos")
